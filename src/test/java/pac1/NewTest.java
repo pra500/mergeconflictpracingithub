ceb221673e1c1dcff22422951a2b5ae757da496e
pragti gupta <pragtig6@gmail.com>
2024-12-16 19:00:34 +0530

    a1

---
+++ src/test/java/pac1/NewTest.java
@@ -0,0 +1,30 @@
+package pac1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	WebDriver driver;
+
+	
+	
+  @Test
+  public void fTest() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.get("https://practicetestautomation.com/practice-test-login/");		
+		System.out.println(driver.getTitle());
+		
+		
+		
+		
+  }
+}
